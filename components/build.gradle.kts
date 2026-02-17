2aa271f943231f4940aa7510027d15694d5b7693
t0mashh <129892614+t0mashh@users.noreply.github.com>
2026-02-17 15:00:58 +0200

    Update build.gradle.kts
    Kotlin и Compose

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -1,28 +1,34 @@
 plugins {
-    id("maven-publish")
+    id("java-library")
+    kotlin("jvm") version "1.9.22"
+    id("org.jetbrains.compose") version "1.6.0"
 }
 
-subprojects {
-    apply(plugin = "maven-publish")
+group = "pro.gravit.launcher.runtime"
+version = project.version
 
-    publishing {
-        repositories {
-            mavenLocal()
-            findProperty("maven.upload.repository")?.let {
-                maven {
-                    url = uri(it)
-                }
-            }
-        }
-        publications {
-            var name = project.name
-            create<MavenPublication>("maven") {
-                groupId = "com.gravitlauncher.launcher"
-                artifactId = name
-                version = project.version as String?
-
-                from(components["java"])
-            }
-        }
+sourceSets {
+    main {
+        java.srcDirs("src/main/java")
+        java.srcDirs("src/main/kotlin") 
+        resources.srcDirs("src/main/resources")
     }
+}
+
+dependencies {
+    api(project(":components:launcher-api"))
+    api(project(":components:launcher-core"))
+    
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material)
+    implementation(compose.material3)
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.3")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8" 
 }
